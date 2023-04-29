a0e2474a2ceac3db439515e010cc1f6d163a9d81
mohamedhisham2212 <104698715+mohamedhisham2212@users.noreply.github.com>
2023-04-30 01:08:56 +0200

    Add files via upload

---
+++ settings/setting.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='UTF-8' />
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css' />
+        <script src='https://code.jquery.com/jquery-1.10.2.js'></script>
+      
+        <link rel='stylesheet' href='<?php echo BASE_URL; ?>assets/css/font-awesome.css' />
+        <link rel='stylesheet' href='<?php echo BASE_URL; ?>assets/css/bootstrap.css' />
+        <link rel='stylesheet' href='<?php echo BASE_URL; ?>assets/css/style-complete.css' />
+        <link rel='stylesheet' href='<?php echo BASE_URL; ?>assets/css/style.css' />
+        <script src='<?php echo BASE_URL; ?>assets/js/jquery-3.1.1.min.js'></script>
+    </head>
+    <body>
+        <div class="popupTweet"></div>
+
+        <?php require '../../right-sidebar.php' ?>
+    </body>
+</html>
